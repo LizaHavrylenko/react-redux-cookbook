3eca8f67cca190b974a1676317ea68d781433038
liza <lizagavrilenkooo@gmail.com>
2018-10-02 22:51:45 +0300

    Add minor fixes in Recipe props

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -6,7 +6,7 @@ export const Recipe  = (props)=> {
     return(
         <div>
         <h2 style = {headerStyles}>{props.title}</h2>
-        <img alt = "Recipe logo" style = {imageStyles} src = {props.state.image}/>
+        <img alt = "Recipe logo" style = {imageStyles} src = {props.image}/>
         <p style = {paragraphStyles}>{props.ingredients}</p>
         <p style = {paragraphStyles}>{props.description}</p>
         <Link to  = {`/recipes/${props.id}/edit`}><button type = "button" className = "btn btn-default"  style = {buttonStyles}>Edit this recipe</button></Link>
