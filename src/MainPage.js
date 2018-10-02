c28cb8ce7570d59aed5812739854c2f6eaa85bb4
liza <lizagavrilenkooo@gmail.com>
2018-10-02 16:22:33 +0300

    Move InfoAboutDelete to components folder

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from '../../../.cache/typescript/2.9/node_modules/@types/react';
 import {IntroToCookbook} from './IntroToCookbook';
 import RecipesList from './RecipesList';
  
