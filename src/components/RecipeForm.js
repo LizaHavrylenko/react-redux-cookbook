98e8aeb55514c2f19c51505442918db2397a9e9b
liza <lizagavrilenkooo@gmail.com>
2018-10-03 12:14:20 +0300

    Add PropTypes to RecipeForm

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 import {imageStyles, imageDivStyles, imageInputStyles, headerStyles, inputStyles, formGroupStyles, buttonStyles} from '../styles';
 
 export const RecipeForm  = (props) => {
 
-return(
-<form>
-<h2 style = {headerStyles}>{props.header}</h2>
-<div style = {imageDivStyles}>
-<input  id = "input"type="file"  onChange={props.handleChangeImage}   style = {imageInputStyles} />
-<img id = "image"  src={props.image} style = {imageStyles} alt = "Recipe logo"/>
-</div>  
-  <div className = "form-group" style = {formGroupStyles}>
-    <label htmlFor = "title" >Title:</label>
-     <input type = "text" id = "title" style = {inputStyles} className = "form-control"  value = {props.title} onChange = {props.handleChangeTitle} />
-  </div>
-  <div className = "form-group" style = {formGroupStyles}>
-    <label htmlFor = "Ingredients">Ingredients:</label>
-    <textarea  type = "text" id = "Ingredients" style = {inputStyles}  className = "form-control" value = {props.ingredients}  onChange = {props.handleChangeIngredients}></textarea>
-  </div>
-  <div className = "form-group" style = {formGroupStyles}>
-    <label htmlFor = "description">Description:</label>
-    <textarea  type = "text" id = "description" style = {inputStyles}  className = "form-control" value = {props.ingredients}  cols = "400" wrap = "hard" onChange = {props.handleChangeDescription}></textarea>
-  </div>
-  <Link to = {`/recipes/${props.title}`}><button type = "submit"  className = "btn btn-default" onClick = {props.handleSubmit}  style = {buttonStyles}>{props.button}</button></Link> 
-  <Link to = "/recipes"><button type = "button" className = "btn btn-default" style = {buttonStyles}>Cancel</button></Link>
-</form>
-)
+          return(
+              <form>
+              <h2 style = {headerStyles}>{props.header}</h2>
+              <div style = {imageDivStyles}>
+              <input  id = "input"type="file"  onChange={props.handleChangeImage}   style = {imageInputStyles} />
+              <img id = "image"  src={props.image} style = {imageStyles} alt = "Recipe logo"/>
+              </div>  
+              <div className = "form-group" style = {formGroupStyles}>
+              <label htmlFor = "title" >Title:</label>
+              <input type = "text" id = "title" style = {inputStyles} className = "form-control"  value = {props.title} onChange = {props.handleChangeTitle} />
+              </div>
+              <div className = "form-group" style = {formGroupStyles}>
+              <label htmlFor = "Ingredients">Ingredients:</label>
+              <textarea  type = "text" id = "Ingredients" style = {inputStyles}  className = "form-control" value = {props.ingredients}  onChange = {props.handleChangeIngredients}></textarea>
+              </div>
+              <div className = "form-group" style = {formGroupStyles}>
+              <label htmlFor = "description">Description:</label>
+              <textarea  type = "text" id = "description" style = {inputStyles}  className = "form-control" value = {props.ingredients}  cols = "400" wrap = "hard" onChange = {props.handleChangeDescription}></textarea>
+              </div>
+              <Link to = {`/recipes/${props.title}`}><button type = "submit"  className = "btn btn-default" onClick = {props.handleSubmit}  style = {buttonStyles}>{props.button}</button></Link> 
+              <Link to = "/recipes"><button type = "button" className = "btn btn-default" style = {buttonStyles}>Cancel</button></Link>
+              </form>
+            )
+}
+
+RecipeForm.propTypes = {
+  header: PropTypes.string,
+  title: PropTypes.string,
+  image: PropTypes.string,
+  ingredients: PropTypes.string,
+  description: PropTypes.string,
+  id: PropTypes.string,
+  handleChangeImage: PropTypes.func,
+  handleChangeDescription: PropTypes.func,
+  handleChangeIngredients: PropTypes.func,
+  handleChangeTitle: PropTypes.func,
+  handleSubmit: PropTypes.func,
 }
  
 
