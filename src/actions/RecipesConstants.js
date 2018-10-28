c8ddff57785fb89f82562019205a418570c7b0e5
liza <lizagavrilenkooo@gmail.com>
2018-10-28 23:06:57 +0200

    Delete unused constants

--- src/actions/RecipesConstants.js
+++ src/actions/RecipesConstants.js
@@ -1,5 +1,4 @@
 export const ADD_RECIPE = "ADD_RECIPE";
 export const UPDATE_RECIPE = "UPDATE_RECIPE";
 export const DELETE_RECIPE = "DELETE_RECIPE";
-export const SHOW_RECIPE = "SHOW_RECIPE";
-export const SHOW_TITLES = "sHOW_TITLES";
+ 
